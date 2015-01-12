dbb038f59ee6c54b31caad8ecf945f99947b4c22
asm582 <asm582@users.noreply.github.com>
2015-01-12 22:28:29 +0530

    Rolled back commit

--- time/test_time_tcp.py
+++ time/test_time_tcp.py
@@ -18,14 +18,16 @@ import integrationtestlib
 import traceback
 import time
 import math
-import nonportable
+
+# use repy helper to bring in tcp_time.repy
+import repyhelper
 
 from repyportability import *
 add_dy_support(locals())
 
-dy_import_module_symbols('tcp_time.r2py')
-dy_import_module_symbols('ntp_time.r2py')
-time = dy_import_module("time_interface.r2py")
+repyhelper.translate_and_import('tcp_time.r2py')
+repyhelper.translate_and_import('ntp_time.r2py')
+
 
 def main():
   # initialize the gmail module
@@ -36,7 +38,7 @@ def main():
 
 
   notify_str = ''
-
+  connected_server = ''
   integrationtestlib.log("Starting test_time_tcp.py...")  
 
   # Whenever we fail to do tcp_time_updatetime, we add the exception string
@@ -44,6 +46,7 @@ def main():
   
   # get the time 5 times and make sure they are reasonably close
   test_start = getruntime()
+  print test_start
   times = []
  
   # Keep a list of servers we connected to or tried to connect to for time.
@@ -53,7 +56,7 @@ def main():
   for i in range (5):
     try:
       connected_server = tcp_time_updatetime(12345)
-      current_time = time.time_gettime()
+      current_time = time_gettime()
       times.append(current_time)
       server_list.append(connected_server)
       integrationtestlib.log("Calling time_gettime(). Retrieved time: " + str(current_time))
@@ -63,8 +66,6 @@ def main():
 
   # Get the stop time 
   test_stop1 = getruntime()
-
-
   # Make sure that we don't fail too many times
   if len(times) < 4:  # more than one attempt failed
     notify_str += "failed to get ntp time via tcp at least twice in 5 attempts\n\n"
@@ -74,35 +75,38 @@ def main():
   fail_test_str = ''
 
   # Find out the difference in time between the start time and end time of the test.
-  diff = max(times) - min(times)
-  test_diff = test_stop1 - test_start
+  #I get a case when times array is null
+  if len(times) != 0:
+    diff = max(times) - min(times)
+    test_diff = test_stop1 - test_start
 
   # If the time difference was more then 10 seconds then there is a problem.
-  if math.fabs(diff - test_diff) > 10:  
-    fail_test_str += ' WARNING large descrepancy between tcp times. \nThe difference between tcp_time diff and the actual test diff is: '+str(diff - test_diff)
-    fail_test_str += "\n\nThe start time of test was: " + str(test_start) + ". The end time of test was: " + str(test_stop1)
-    fail_test_str += "\n\nThe list of times returned by the tcp_server were: " + str(times)
-    fail_test_str += "\n\nThe max time was: " + str(max(times)) + ". The min time was: " + str(min(times))
-    fail_test_str += ". \n\nThe tcp diff time was: " + str(diff) + ". This time should have been less then: " + str(test_diff)
-    fail_test_str += "\n\nThe servers we connected to are: " + str(server_list) 
-    notify_str += fail_test_str
-    integrationtestlib.log(fail_test_str)
+    if math.fabs(diff - test_diff) > 10:  
+      fail_test_str += ' WARNING large descrepancy between tcp times. \nThe difference between tcp_time diff and the actual test diff is:    '+str(diff - test_diff)
+      fail_test_str += "\n\nThe start time of test was: " + str(test_start) + ". The end time of test was: " + str(test_stop1)
+      fail_test_str += "\n\nThe list of times returned by the tcp_server were: " + str(times)
+      fail_test_str += "\n\nThe max time was: " + str(max(times)) + ". The min time was: " + str(min(times))
+      fail_test_str += ". \n\nThe tcp diff time was: " + str(diff) + ". This time should have been less then: " + str(test_diff)
+      fail_test_str += "\n\nThe servers we connected to are: " + str(server_list) 
+      notify_str += fail_test_str
+      integrationtestlib.log(fail_test_str)
     integrationtestlib.log("Local time diff: " + str(local_end_time - local_start_time))    
 
   # Now do an ntp time test
   try:
     ntp_time_updatetime(12345)
-    ntp_t = time.time_gettime()
+    ntp_t = time_gettime()
   except Exception,e:
     integrationtestlib.log("Failed to call ntp_time_updatetime(). Returned with exception: " +str(e))
     notify_str += "\nFailed to call ntp_time_updatetime(). Returned with exception: " +str(e)
   
   test_stop2 = getruntime()
-  diff = ntp_t - max(times)
-  if diff > (8 + test_stop2 - test_stop1):
-    exceedby = diff - (test_stop2-test_stop1)
-    integrationtestlib.log('WARING large descrepancy between ntp and tcp times')
-    notify_str += ' WARNING large descrepancy between ntp and tcp time: '+str(exceedby)
+  if len(times) !=0:
+    diff = ntp_t - max(times)
+    if diff > (8 + test_stop2 - test_stop1):
+      exceedby = diff - (test_stop2-test_stop1)
+      integrationtestlib.log('WARING large descrepancy between ntp and tcp times')
+      notify_str += ' WARNING large descrepancy between ntp and tcp time: '+str(exceedby)
 
   
   if notify_str != '':
